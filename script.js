8864ad03dbda1860b7e444979c9992c39945a522
andika.pelealu90 <andika.pelealu90@gmail.com>
2020-09-05 01:45:07 +0700

    working on script

--- script.js
+++ script.js
@@ -1,41 +1,84 @@
-//jQuery
- 
-$(document).ready(function (){
-  
-    //Variable declarations that use or refer to jquery should be INSIDE the ready function to make sure jquery is instanciated. 
-    $name = $('input[name="item-name"'); //notice the different declaration…
-    $amount = $('input[name="amount"'); 
-    $type = $('select[name="type"');
-    $date = $('input[name="date"');
-    
-    
-    //Notice the $varname - Now each of these is is a jquery object corresponding to the DOM element and can be used to call the .val() method directly at any time. The way you had it before would only grab the value at the time of the declarations - ie before the values are set.
-    
-    $('#button').click(function () {
-      // Making it always add a icon in the type row
-      var type_icon; //Store the html fragment to a variable when needed.
-      if ($type.val() == 'card') {
-        type_icon = "<i class='fa fa-credit-card'></i>";
-      } else if ($type.val() == 'cash') {
-        type_icon = "<i class='fa fa-money'></i>";
-      } else if ($type.val() == 'cryptocoin') {
-        type_icon = "<i class='fa fa-bitcoin'></i>";
-      } else if ($type.val() == 'other') {
-        type_icon = "<i class='fa fa-question-circle'></i>";
-      }
-      
-      var form_validated = true;
-      //Add your validation tests, have them set form_validated to false if they fail
-      //TODO
-      
-      if(form_validated) {
-        //Now we can use our references we made before
-        $("table tr:first").after('<tr><td>'+type_icon+'</td><td>'+$name.val()+'</td><td>'+formatDate($date.val())+'</td><td class="amount">$'+$amount.val()+'</td></tr>');
-        $date.val(null);
-        $amount.val(null);
-        $name.val(null);
-        $("#if-empty").remove();
-      };
-    });
-    
-  });
+// # DOM Exercise
+
+// **Background**
+// Mr. James decided to make a notes about his Income and Expenses every month.
+
+// ### Main Task
+// **Make Income and Expenses Apps**
+
+// ## Task 1 : Prepare variable 
+// Prepare global variable for data
+// ```javascript
+    // var posts = []; //isi : object income dan outcome
+    var pemasukan = []; // isi : id : number, title : string, type:string, total:number
+    var pengeluaran = []; // isi : id : number, title: string, type:string, total:number
+
+    function addData(title,type,total) {
+
+        if (type == "pemasukan") {
+            let a = {
+                title: title,
+                type: type,
+                total: total,
+            }
+            pemasukan.push(a)
+
+
+        } else if (type == "pengeluaran") {
+
+            let a = {
+                title: title,
+                type: type,
+                total: total,
+            }
+            pengeluaran.push(a)
+        } else {
+            return "Invalid Data Type"
+        }            
+
+        
+    }
+    // addData("masak","pemasukan",50000)
+    // addData("gaji","pemasukan",5000000)
+    // addData("susu","pengeluaran",150000)
+
+
+    // console.log(pengeluaran);
+// ## Task 2 : getData
+// Code getData function to read the data from the global variable
+// ```javascript
+    function getData(get){
+        for (let i = 0; i < get.length; i++) {
+            // console.log(pengeluaran[i]);
+            if (get == "pengeluaran") {
+                return pengeluaran
+            }
+            else if (get == "pemasukan") {
+                return pemasukan
+            }
+            return "Data tidak ditemukan";
+            }
+    }
+    // console.log(getData('pengeluaran')); 
+    // console.log(getData('pemasukan')); 
+
+
+// ```
+
+// ## Task 4 : deleteData
+// User can delete post
+// ```javascript
+    function deleteData(type,id){
+        if (type == "pemasukan") {
+            pemasukan = delete pemasukan.id;
+            return pemasukan
+        }
+        else if (type == "pengeluaran") {
+            pengeluaran = delete pengeluaran.id;
+            return pemasukan
+    }
+}
+
+// ```
+// ## Task 5 : Split the files
+// Developer can split the files (module.exports and require)
